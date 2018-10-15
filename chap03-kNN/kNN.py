1aa9a0c06b6e32711401dcf2da2d6510a4eb1b36
godelscat <shimrac@outlook.com>
2018-10-15 14:41:54 +0200

    fix indent

--- chap03-kNN/kNN.py
+++ chap03-kNN/kNN.py
@@ -11,49 +11,50 @@ from kdTree import KDTree
 
 class KNN():
 	
-	def __init__(self, k=5):
-		self.k = k
-		self.tree = KDTree()
-	
-	def _max_label(self, k_list):
-		temp = [x.label for x in k_list]
-		max_occ = Counter(temp).most_common(1)
-		max_label = max_occ[0][0]
-		return max_label 
-	
-	def train(self, data, labels):
-		assert data[:,0].size == labels.size
-		self.tree.build(data, labels)	
-		
-	def predict(self, data, labels):
-		assert data[:,0].size == labels.size
-		nevents = labels.size
-		counts = 0
-		for i in range(nevents):
-			k_list = []
-			k_list = self.tree.kNN_points(data[i], self.k)	
-			la = self._max_label(k_list)
-			if la == labels[i]:
-				counts += 1
-				print(counts)
-		accuracy = format(counts/nevents, '.5f')
-		print("kNN Model accuracy is : {}".format(accuracy))
+    def __init__(self, k=5):
+        self.k = k
+        self.tree = KDTree()
+    
+    def _max_label(self, k_list):
+        temp = [x.label for x in k_list]
+        max_occ = Counter(temp).most_common(1)
+        max_label = max_occ[0][0]
+        return max_label 
+    
+    def train(self, data, labels):
+        assert data[:,0].size == labels.size
+        self.tree.build(data, labels)	
+            
+    def predict(self, data, labels):
+        assert data[:,0].size == labels.size
+        nevents = labels.size
+        counts = 0
+        for i in range(nevents):
+            k_list = []
+            k_list = self.tree.kNN_points(data[i], self.k)	
+            la = self._max_label(k_list)
+            if la == labels[i]:
+                counts += 1
+                print(counts)
+        accuracy = format(counts/nevents, '.5f')
+        print("kNN Model accuracy is : {}".format(accuracy))
 
 
 if __name__ == "__main__":
 
-	raw_data = pd.read_csv("../dataset/MNIST/train.csv")
-	labels = raw_data['label'].values
-	data = raw_data.iloc[:,1:].values
-	train_data, train_labels, test_data, test_labels = train_split(data, labels)
+    raw_data = pd.read_csv("../dataset/MNIST/train.csv")
+    labels = raw_data['label'].values
+    data = raw_data.iloc[:,1:].values
+    train_data, train_labels, test_data, test_labels = train_split(data, labels)
 
-	# due to python recursion limit, I have to cut down # of train and test data
-	print("start building model")
-	model = KNN(k=10)
-	print("start training")
-	model.train(train_data, train_labels)
-	print("start predicting")
-	model.predict(test_data, test_labels)
-	print("predicting ends")
-	# final accuracy is around 95% 
-	# cost about 17min in my computer
+    # due to python recursion limit, I have to cut down # of train and test data
+    print("start building model")
+    print("Hello")
+    model = KNN(k=10)
+    print("start training")
+    model.train(train_data, train_labels)
+    print("start predicting")
+    model.predict(test_data, test_labels)
+    print("predicting ends")
+    # final accuracy is around 95% 
+    # cost about 17min in my computer
