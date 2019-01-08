17e85a958a773acfb3f45473c8bd02c3c7f22bbb
godelscat <shimrac@outlook.com>
2019-01-08 19:56:09 +0800

    typo

--- DNN/NaiveLayer.py
+++ DNN/NaiveLayer.py
@@ -85,16 +85,18 @@ class Affine:
     def __init__(self, w, b):
         self.W = w
         self.b = b
+        self.x = None
         self.dW = None
         self.db = None
 
     def forward(self, x):
-        out = np.dot(x, self.W) + self.b
+        self.x = x
+        out = np.dot(self.x, self.W) + self.b
         return out 
     
     def backward(self, dout):
         dx = np.dot(dout, self.W.T)
-        self.dW = np.dot(self.W.T, dout)
+        self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
         return dx
 
